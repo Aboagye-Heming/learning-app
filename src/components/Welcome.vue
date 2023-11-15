f17fb21bae7b5904c55d2efc41fdba8bcf3bc82e
Aboagye=Heming <hemingkwabena@gmail.com>
2023-11-15 08:46:40 +0000

    created task store

---
+++ src/components/Welcome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="item">
+    <header>
+      <h1>Pinia Tasks</h1>
+    </header>
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" key="tasks.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useTaskStore } from "../store/TaskStore";
+import TaskDetails from "./TaskDetails.vue";
+
+export default {
+  components:{TaskDetails},
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
