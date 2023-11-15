8877c55a0385aeef23738f3cb30341ba9f873d6b
Aboagye=Heming <hemingkwabena@gmail.com>
2023-11-15 09:39:08 +0000

    added total number of tasks

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,8 +3,24 @@
     <header>
       <h1>Pinia Tasks</h1>
     </header>
-    <div class="task-list">
+
+    <!-- filter nav -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All Tasks</button>
+      <button @click="filter = 'favs'">Favorite Tasks</button>
+    </nav>
+    
+
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount  }} tasks left to do</p>
       <div v-for="task in taskStore.tasks" key="tasks.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favsCount  }} favorite tasks left to do</p>
+
+      <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -14,13 +30,15 @@
 <script>
 import { useTaskStore } from "../store/TaskStore";
 import TaskDetails from "./TaskDetails.vue";
+import { ref } from "vue";
 
 export default {
-  components:{TaskDetails},
+  components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore();
+    const filter = ref("all");
 
-    return { taskStore };
+    return { taskStore, filter};
   },
 };
 </script>
--- src/store/TaskStore.js
+++ src/store/TaskStore.js
@@ -3,11 +3,21 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [
-      { id: 1, title: "buy some milk", isFav: false },
-      { id: 2, title: "play football", isFav: true },
+      { id: 1, title: "Buy some milk", isFav: false },
+      { id: 2, title: "Play football", isFav: true },
     ],
-   }),
+  }),
+  getters: {
+    favs() {
+      return this.tasks.filter((t) => t.isFav);
+    },
+    favsCount() {
+      return this.tasks.reduce((p, c) => {
+        return c.isFav ? p + 1 : p;
+      }, 0);
+    },
+    totalCount: (state) => {
+      return state.tasks.length
+    },
+  },
 });
-
-
-
